d2b0df75159a7ff97f1182188ba1039e92de1ac5
spaghettyii <hyunwooyi2@gmail.com>
2025-12-12 20:32:30 -0500

    initial commit, making day1

---
+++ day1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char** argv)
+{
+    FILE *fptr = fopen("./input.txt", "r");
+    FILE *fptr = fopen("./output.txt", "w");
+}
