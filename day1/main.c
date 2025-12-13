4c28920017371a544e9332052805c2f64b2424b5
spaghettyii <hyunwooyi2@gmail.com>
2025-12-12 21:00:20 -0500

    solved day1

--- day1/main.c
+++ day1/main.c
@@ -4,6 +4,30 @@
 
 int main (int argc, char** argv)
 {
-    FILE *fptr = fopen("./input.txt", "r");
-    FILE *fptr = fopen("./output.txt", "w");
+    FILE *inputFile = fopen("./input.txt", "r");
+    FILE *outputFile = fopen("./output.txt", "w");
+    if (inputFile == NULL || outputFile == NULL) {
+        printf("oopsies\n");
+        return -1;
+    }
+    printf("meow\n");
+
+    char line[5];
+    int dial = 50, sum = 0;
+    while (fgets(line, 6, inputFile) != NULL)
+    {
+        char dir = line[0];
+        int move = atoi(&line[1]);
+        if (dir == 'L') 
+            dial = (dial - move) % 100;
+        else 
+            dial = (dial + move) % 100;
+        if (dial == 0) sum++;
+    }
+    printf("sum: %d\n", sum);
+
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
 }
